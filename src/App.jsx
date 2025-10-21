19d078bca2110f8bb8250f7a978de756d11b3739
Vivien <bihuiy7@gmail.com>
2025-10-21 13:58:21 +1100

    add more google analytics into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
+// * Page components related
 import { Route, Routes } from "react-router";
-
 import Navbar from "./compomnents/Navbar/Navbar";
 import Homepage from "./compomnents/Homepage/Homepage";
 import ResumePage from "./compomnents/ResumePage/ResumePage";
 import ProjectPage from "./compomnents/ProjectPage/ProjectPage";
 import ContactPage from "./compomnents/ContactPage/ContactPage";
 
+// * Google analytics related
+import ReactGA from "react-ga4";
+import { useEffect } from "react";
+import { useLocation } from "react-router";
+
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.initialize("G-VV6BQRZB7F");
+  }, []);
+
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
+
   return (
     <>
       <Navbar />
